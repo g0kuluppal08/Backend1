78c33c844f51de2ad192b1cf5830a0cecbc8740b
g0kuluppal08 <97520505+g0kuluppal08@users.noreply.github.com>
2024-10-17 04:11:09 +0530

    dome somethign

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const bookingRoutes = require('./routes/bookingRoute');
+const courtsRoutes = require('./routes/courtRoutes');
+const centreRoutes = require('./routes/centreRoutes');
+import path from 'path';
+const cors=require('cors');
+dotenv.config();
+const app = express();
+connectDB();
+
+
+// Middleware to parse incoming request bodies
+app.use(bodyParser.json());
+
+app.use(
+  cors({
+    origin: process.env.RONT_END_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use('/api/auth', authRoutes);
+app.use('/api/admin/bookings',bookingRoutes);
+app.use('/api/admin/courts',courtsRoutes);
+app.use('/api/admin/centres', centreRoutes);
+app.use('/api/bookings', bookingRoutes);
+app.use('/api/courts',courtsRoutes);
+app.use('/api/centres', centreRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
